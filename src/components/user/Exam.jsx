c42b33e6d4947fa624361e913328495b56071b83
Eduardo Segura Richart <edusr@correo.ugr.es>
2022-05-26 00:43:12 +0200

    Añade boton finalizar

--- src/components/user/Exam.jsx
+++ src/components/user/Exam.jsx
@@ -1,17 +1,31 @@
-import { ScrollView, Text, TextInput, Image, StyleSheet } from 'react-native'
+import { ScrollView, View, StyleSheet } from 'react-native'
 import QuestionCard from './QuestionCard'
-import UI_COLORS from '../../colors'
+import NextButton from '../signup/NextButton'
 
 const Exam = (props) => {
+    const handleOnPress = () => {
+
+    }
 
     return (
         <ScrollView>
-            <QuestionCard />
-            <QuestionCard />
-            <QuestionCard />
-            <QuestionCard />
+            <QuestionCard {...{ number: 1 }}/>
+            <QuestionCard {...{ number: 2 }}/>
+            <QuestionCard {...{ number: 3 }}/>
+            <QuestionCard {...{ number: 4 }}/>
+            <View style={styles.container}>
+                <NextButton {...{ label: 'Finalizar', handleOnPress }} />
+            </View>
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 12,
+        marginHorizontal: 24,
+        marginBottom: 32
+    }
+})
 
 export default Exam
